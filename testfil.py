98dce8f06bc7c763e188f93616363e0e437e5360
Michael H <micke._.23@hotmail.com>
2025-10-08 10:13:16 +0200

    sparat

--- testfil.py
+++ testfil.py
@@ -1,17 +1,1 @@
-import Item
-import Inventory
-from items import Item
-from inventory import Inventory
-
-inv = Inventory()
-inv.add(Item("Sword", "weapon", 10))
-inv.add(Item("Shield", "armor", 5))
-inv.add(Item("Potion", "potion", 2))
-
-inv.show()
-
-inv.remove_by_name("Potion")
-inv.show()
-
-print("Has sword?", inv.has("sword"))
-print("Count:", inv.count())
+print('Hello')
